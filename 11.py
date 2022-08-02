4ed7be3cc98f04c0c0e0a43edc814550a1a9e744
ChintanPatel310 <patelchintan3101@gmail.com>
2022-08-02 23:23:58 +0530

    First Commit

--- 11.py
+++ 11.py
@@ -1,51 +1,48 @@
-#print('how are you?')
-#print('hello', end=' ')
-"""
-print('hello', end='!')
-print('good morning')
-"""
-#print('at 10:30','good night')
-'''
-print('\narrow')
-print('\\narrow')
-'''
+print('how are you?')        #printing statement
+print('hello', end=' ')      #add 'hello' in new line
 
-#escape_
-'''
-print('a:\namzing!')
+print('hello', end='!')      #print 'hello' with '!'
+print('good morning')        #print along with 'hello!'
+
+print('at 10:30','good night')  #print in sequence
+
+
+print('\narrow')       #/n use while we print new line (alert when we use this type of words)
+print('\\narrow')      #otherwise
+
+
+ #escape_
+
+print('a:\namzing!')            #escape statement
 print("a:\'amazing!'")
-print('nice \n good \t12')
-'''
-#print('hi, i\'m from lunawada') 
-#print(""chintan"")_show the error
-#print("\"chintan\"")_correct way to write in inverted form
+print('nice \n good \t12')      #tab and new line character
+
+print('hi, i\'m from lunawada') 
+print(""chintan"")               #show the error
+print("\"chintan\"")             #correct way to write in inverted form
 
 
-#varibles
+ #varibles
 
 
-'''
 a=10
-#b='good boy ' #b=good boy_don't write
+b='good boy' #b=good boy_don't write
 b='20'
 c=25.5
 d="50"
 print(b)
-'''
-'''
 print(type(c))
 print(type(a))
 print(type(b))
-'''
-'''
+
 print(a+c)
-#print(a+b)
-#print(b+d)
-#print(int(b)+int(d))
-#print(10*'hello')
-#print(10*'hello\n')
+print(a+b)
+print(b+d)
+print(int(b)+int(d))
+print(10*'hello')
+print(10*'hello\n')
 print(10*str(int(b)+int(d)))
-'''
+
 
 #two numbers arithmatic operations
 a = int(input("input :"))
